c82110285b3b1e1b2f0f64ba3ffd419ea20c4426
Cooper <cooper.macgregor14@gmail.com>
2023-08-14 23:14:45 -0400

    only have the conversation madel displayed as i dont want to pay for more models

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -20,34 +20,34 @@ const tools = [
     bgColor: "bg-violet-500/10",
     href: "/conversation",
   },
-  {
-    label: "Music Generation",
-    icon: Music,
-    color: "text-emerald-500",
-    bgColor: "bg-emerald-500/10",
-    href: "/music",
-  },
-  {
-    label: "Image Generation",
-    icon: ImageIcon,
-    color: "text-pink-700",
-    bgColor: "bg-pink-700/10",
-    href: "/image",
-  },
-  {
-    label: "Video Generation",
-    icon: VideoIcon,
-    color: "text-orange-700",
-    bgColor: "bg-orange-700/10",
-    href: "/video",
-  },
-  {
-    label: "Code Generation",
-    icon: Code,
-    color: "text-green-700",
-    bgColor: "bg-green-700/10",
-    href: "/code",
-  },
+  // {
+  //   label: "Music Generation",
+  //   icon: Music,
+  //   color: "text-emerald-500",
+  //   bgColor: "bg-emerald-500/10",
+  //   href: "/music",
+  // },
+  // {
+  //   label: "Image Generation",
+  //   icon: ImageIcon,
+  //   color: "text-pink-700",
+  //   bgColor: "bg-pink-700/10",
+  //   href: "/image",
+  // },
+  // {
+  //   label: "Video Generation",
+  //   icon: VideoIcon,
+  //   color: "text-orange-700",
+  //   bgColor: "bg-orange-700/10",
+  //   href: "/video",
+  // },
+  // {
+  //   label: "Code Generation",
+  //   icon: Code,
+  //   color: "text-green-700",
+  //   bgColor: "bg-green-700/10",
+  //   href: "/code",
+  // },
 ];
 const DashboardPage = () => {
   const router = useRouter();
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -30,35 +30,35 @@ const routes = [
     href: "/conversation",
     color: "text-violet-500",
   },
-  {
-    label: "Image Generation",
-    icon: ImageIcon,
-    href: "/image",
-    color: "text-pink-700",
-  },
-  {
-    label: "Video Generation",
-    icon: VideoIcon,
-    href: "/video",
-    color: "text-orange-700",
-  },
-  {
-    label: "Music Generation",
-    icon: Music,
-    href: "/music",
-    color: "text-emerald-500",
-  },
-  {
-    label: "Code Generation",
-    icon: Code,
-    href: "/code",
-    color: "text-green-700",
-  },
-  {
-    label: "Settings",
-    icon: Settings,
-    href: "/settings",
-  },
+  //   {
+  //     label: "Image Generation",
+  //     icon: ImageIcon,
+  //     href: "/image",
+  //     color: "text-pink-700",
+  //   },
+  //   {
+  //     label: "Video Generation",
+  //     icon: VideoIcon,
+  //     href: "/video",
+  //     color: "text-orange-700",
+  //   },
+  //   {
+  //     label: "Music Generation",
+  //     icon: Music,
+  //     href: "/music",
+  //     color: "text-emerald-500",
+  //   },
+  //   {
+  //     label: "Code Generation",
+  //     icon: Code,
+  //     href: "/code",
+  //     color: "text-green-700",
+  //   },
+  //   {
+  //     label: "Settings",
+  //     icon: Settings,
+  //     href: "/settings",
+  //   },
 ];
 
 const Sidebar = () => {
